2a339dad191e74087eb3e4849b3891bc7580c41b
vaibhavthorat115@gmail.com <vaibhavthorat115@gmail.com>
2023-08-28 12:51:47 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Component/Navbar';
+import Footer from './Component/Footer';
+import Main from './Component/main/Main';
+import { Route, Routes } from 'react-router-dom';
+import ProductDetails from './Component/ProductDetails';
+import PageNotFound from './Component/PageNotFound';
+import ScrollToTop from './Component/ScrollToTop';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Navbar></Navbar>
+      <Routes>
+        <Route exact path='productdetails' element={<ProductDetails></ProductDetails>}></Route>
+        <Route exact path='/' element={<Main></Main>}></Route>
+        <Route exact path='/home' element={<Main></Main>}></Route>
+        <Route exact path="/productdetails/:id" element={<ProductDetails></ProductDetails>}></Route>
+        <Route exact path='*' element={<PageNotFound></PageNotFound>}></Route>
+      </Routes>
+        <Footer></Footer>
+        <ScrollToTop />
     </div>
   );
 }
